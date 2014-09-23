43407f4cc48d5423a8284a8069ee55b828051509
aneta <dragon21century@gmail.com>
2014-09-23 10:18:52 +0900

    Update boogy.cc

--- boogy.cc
+++ boogy.cc
@@ -180,6 +180,8 @@ int main (int argc, char *argv[])
   LinuxStackHelper::RunIp (nodes.Get (0), Seconds (11), "link set down dev sim1");
   LinuxStackHelper::RunIp (routers.Get (1), Seconds (11), "link set down dev sim0");
   
+  LinuxStackHelper::RunIp (nodes.Get (0), Seconds (13), "link set up dev sim1");
+  LinuxStackHelper::RunIp (routers.Get (1), Seconds (13), "link set up dev sim0");
   if2 = address2.Assign (devices2);
   address2.NewNetwork ();
       // setup ip routes
@@ -196,9 +198,8 @@ int main (int argc, char *argv[])
       cmd_oss << "route add 10.2.2.0/24 via " << if2.GetAddress (1, 0) << " dev sim1";
       LinuxStackHelper::RunIp (routers.Get (1), Seconds (14), cmd_oss.str ().c_str ());
       
-  LinuxStackHelper::RunIp (nodes.Get (0), Seconds (15), "link set up dev sim1");
-  LinuxStackHelper::RunIp (routers.Get (1), Seconds (15), "link set up dev sim0");
-  LinuxStackHelper::RunIp (nodes.Get (0), Seconds (15), "route add default via 10.1.0.2 dev sim0");
+  
+  LinuxStackHelper::RunIp (nodes.Get (0), Seconds (14), "route add default via 10.1.0.2 dev sim0");
   
 
   Simulator::Stop (Seconds (200.0));
